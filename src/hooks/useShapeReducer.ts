79b70a04d7c9dbcad20685f99d24fb963409a8dc
matsu <donguri47@gmail.com>
2022-06-03 08:36:57 +0900

    コメント追加

--- src/hooks/useShapeReducer.ts
+++ src/hooks/useShapeReducer.ts
@@ -4,19 +4,23 @@ export const useShapeReducer = (calcFunc: any, calcRatio: any) => {
   const shapeReducer = (state: shapeType, action: actionType) => {
     switch (action.type) {
       case 'WIDTH':
+        // 値として計算式を入力した時の結果（計算でない場合、元の値を返す）
         const objScaleWidth = calcFunc(state, action.width, action.type);
 
+        //　縦横比にチェックが付いていたら
         if (action.scaleFlag) {
           objScaleWidth.height = calcRatio(state, objScaleWidth, action.type);
         }
-
         const calcObjWidth = {
           scale: objScaleWidth,
           scaleFlag: action.scaleFlag,
         };
         return calcObjWidth;
       case 'HEIGHT':
+        // 値として計算式を入力した時の結果（計算でない場合、元の値を返す）
         const objScaleHeight = calcFunc(state, action.height, action.type);
+
+        //　縦横比にチェックが付いていたら
         if (action.scaleFlag) {
           objScaleHeight.width = calcRatio(state, objScaleHeight, 'HEIGHT');
         }
